7303cac29a761d6d792c64672de2e8c24e178614
DirtyDev <104915841+TheDirtyDev@users.noreply.github.com>
2026-02-17 17:19:46 -0500

    Update version from 1.2 to 1.3

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,12 @@ game 'gta5'
 
 author 'DirtyDev'
 description 'Discord Rich Presence Script'
-version '1.2'
+version '1.3'
 
 client_scripts {
     'client.lua'
+}
+
+server_scripts {
+    'server.lua'
 }
