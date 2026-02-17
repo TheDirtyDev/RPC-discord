47746bc51177d20fc4ee73541378d1886f54e502
DirtyDev <104915841+TheDirtyDev@users.noreply.github.com>
2026-02-17 17:52:34 -0500

    Simplify GlobalState set for player indices

--- server.lua
+++ server.lua
@@ -1,10 +1,6 @@
 CreateThread(function()
     while true do
         Wait(10000) -- update every 10 seconds
-
-        GlobalState:set('rpc', {
-            GetNumPlayerIndices(),
-            0 -- Don't change this value
-        }, true)
+        GlobalState:set('rpc', { GetNumPlayerIndices() }, true)
     end
 end)
